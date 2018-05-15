5d654ebb02d10562bf8e2e624d3fc2dc6299d1b6
srigeetha <srigeethakanikanti@gmail.com>
2018-05-15 17:30:24 +0530

    ngrouter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { InputComponent } from './input/input.component';
 import { DataComponent } from './data/data.component';
@@ -17,38 +14,30 @@ import { MessagesComponent } from './messages/messages.component';
 import { MultimediaComponent } from './multimedia/multimedia.component';
 import { DragndropComponent } from './dragndrop/dragndrop.component';
 import { MiscComponent } from './misc/misc.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { RadioButtonModule,CheckboxModule,SplitButtonModule } from 'primeng/primeng';
 
-import {CommonModule} from '@angular/common';
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dash', component: DashboardComponent,children:[
+  { path: 'button', component:ButtonsComponent },
+  { path: 'input', component: InputComponent },
+  { path: 'data', component: DataComponent },
+  { path: 'panel', component: PanelComponent },
+  { path: 'over', component:OverlayComponent },
+  { path: 'file', component: FileComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'chart', component: ChartsComponent },
+  { path: 'message', component:MessagesComponent },
+  { path: 'multi', component: MultimediaComponent },
+  { path: 'drag', component: DragndropComponent },
+  { path: 'misc', component: MiscComponent }
+]},
+  
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    ButtonsComponent,
-    InputComponent,
-    DataComponent,
-    PanelComponent,
-    OverlayComponent,
-    FileComponent,
-    MenuComponent,
-    ChartsComponent,
-    MessagesComponent,
-    MultimediaComponent,
-    DragndropComponent,
-    MiscComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule,       
-    AngularFontAwesomeModule,
-    CheckboxModule,
-    RadioButtonModule,
-    SplitButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:[
+    RouterModule.forRoot(routes)
+   ],
+   exports: [RouterModule]
 })
-export class AppModule { }
+
+export class AppRoutingModule { }
